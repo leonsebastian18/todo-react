b4507f08e978d04e7f9ce9f85505f59f96442b59
Sebastian Leon <sebastianleonvillarreal@gmail.com>
2023-12-31 02:24:33 -0500

    se realizaron todos los componetes y la logica del TODO

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import { TodoProvider } from '../TodoContext';
+import { AppUI } from './AppUI';
+import React from 'react';
+
+
+function App() {
+  return(
+    <TodoProvider>
+      <AppUI/>
+    </TodoProvider>
+  );
+}
+
+
+export default App;
